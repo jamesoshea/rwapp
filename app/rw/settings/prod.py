f4991b5f9dd60cdf63fb81c781ba671f28b1c00e
James O'Shea <j.oshea@holidaypirates.com>
2019-05-04 14:23:13 +0200

    build: add digitalocean droplet to allowed_hosts

--- app/rw/settings/prod.py
+++ app/rw/settings/prod.py
@@ -1,6 +1,8 @@
+import os
 from rw.settings.base import *
 
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
@@ -9,6 +11,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
 
 SECRET_KEY = '&o23hjenf0943ht98nvGASFgi#S%""/DN§F(NCQ37164'
 
-ALLOWED_HOSTS = {
-	
-}
+ALLOWED_HOSTS = [
+	'178.128.197.252'
+]
