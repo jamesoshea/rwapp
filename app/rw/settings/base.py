06bbeb69a6b818aa89db54aa7013d8997dac87a6
James O'Shea <j.oshea@holidaypirates.com>
2019-07-08 22:47:42 +0200

    build: simplify static dirs some more

--- app/rw/settings/base.py
+++ app/rw/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = False
 
 ALLOWED_HOSTS = ['jakobschult.pythonanywhere.com',
-                 '127.0.0.1', '178.128.197.252', '167.71.46.59', '0.0.0.0']
+                 '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
