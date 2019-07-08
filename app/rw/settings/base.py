3b1601e41d1ee71022791091805a238d1766ed3e
James O'Shea <j.oshea@holidaypirates.com>
2019-07-08 20:52:29 +0200

    misc: add static files from root folder

--- app/rw/settings/base.py
+++ app/rw/settings/base.py
@@ -117,8 +117,6 @@ AUTH_USER_MODEL = 'fff.User'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# Your project will probably also have static assets that aren’t tied to a particular app.
-# In addition to using a static/ directory inside your apps, you can define a list of directories (STATICFILES_DIRS) in your settings file where Django will also look for static files. For example:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '../static')]
 
 MEDIA_URL = '/media/'
